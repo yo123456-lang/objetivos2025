7c553c609936a4c257e04203f09b49e5a28cc3a8
yo123456-lang <isaialcocer@outlook.es>
2025-04-26 23:51:05 -0600

    Add files via upload

---
+++ carlitos/js/script.js
@@ -0,0 +1,5 @@
+// Mostrar/ocultar el menú en pantallas pequeñas
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    var menu = document.getElementById("nav-menu");
+    menu.classList.toggle("active");
+});
